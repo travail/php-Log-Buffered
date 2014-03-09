0fc3337c5eff15a618521c749475648de926f412
travail <sawa.tomoyuki@gmail.com>
2014-03-09 22:46:09 +0900

    Add tests

---
+++ tests/functional/new_test.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @group functional
+ */
+class FunctionalNewTest extends PHPUnit_Framework_TestCase
+{
+    public function testNew()
+    {
+        try {
+            $logger = new \Log\Buffered();
+        }
+        catch (\Exception $e) {
+        }
+
+        $this->assertTrue($logger instanceof \Log\Buffered);
+    }
+
+    public function testNewWithParameter()
+    {
+        $file = '/tmp/log-buffered.log';
+        try {
+            $logger = new \Log\Buffered(
+                array(
+                    'debug'       => true,
+                    'log_level'   => 'debug',
+                    'trace_level' => 1,
+                    'file'        => $file,
+                    'buffer_size' => \Log\Buffered::DEFAULT_BUFFER_SIZE,
+                )
+            );
+        }
+        catch (\Exception $e) {
+        }
+        unlink($file);
+
+        $this->assertTrue($logger instanceof \Log\Buffered);
+    }
+
+    public function testNewWithInvalidFile()
+    {
+        $logger    = '';
+        $file      = '/tmp/log-buffered-unreadable.log';
+        $exception = '';
+        touch($file);
+        chmod($file, 0444);
+        try {
+            $logger = new \Log\Buffered(
+                array(
+                    'debug'       => true,
+                    'log_level'   => 'debug',
+                    'trace_level' => 1,
+                    'file'        => $file,
+                    'buffer_size' => \Log\Buffered::DEFAULT_BUFFER_SIZE,
+                )
+            );
+        }
+        catch (\Exception $e) {
+            $exception = $e;
+        }
+        unlink($file);
+
+        $this->assertTrue($e instanceof \Exception);
+        $this->assertFalse($logger instanceof \Log\Buffered);
+    }
+}
