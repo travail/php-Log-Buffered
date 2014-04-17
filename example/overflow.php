ffa630b7bfbb879e5a203fbe7ecebbec5f2ccf54
travail <sawa.tomoyuki@gmail.com>
2014-04-17 23:45:00 +0900

    Tidy up README.md and example scripts refs #435: Tidy up README.md

--- example/overflow.php
+++ example/overflow.php
@@ -1,5 +1,7 @@
 <?php
 
+use \Log\Buffered;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 main();
@@ -7,9 +9,9 @@ exit;
 
 function main()
 {
-    $log = new \Log\Buffered(
+    $log = new Buffered(
         array(
-            'buffer_size' => \Log\Buffered::MIN_BUFFER_SIZE,
+            'buffer_size' => Buffered::MIN_BUFFER_SIZE,
         )
     );
     for ($i = 0; $i < 10; $i++) {
