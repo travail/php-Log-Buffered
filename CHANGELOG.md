97274f2f89948088ee566be5b78b68ff7c79664d
travail <sawa.tomoyuki@gmail.com>
2022-08-31 11:51:02 +0900

    Fix documents and Bump up the version to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 CHANGELOG
 ========
 
+## 0.2.0 2020-08-31
+
+* Fix the deprecated feature
+  * Passing null to non-nullable parameters of built-in functions
+* Fix the document
+
 ## 0.1.0 2014-05-01
 
 * Composerized
--- lib/Buffered.php
+++ lib/Buffered.php
@@ -2,32 +2,34 @@
 
 namespace Log;
 
+use InvalidArgumentException;
+
 class Buffered extends Minimal
 {
     /**
      * @var string The version of this package
      */
-    const VERSION = '0.1.0';
+    public const VERSION = '0.2.0';
 
     /**
      * @var int Min buffer size
      */
-    const MIN_BUFFER_SIZE = 1024; // 1K
+    public const MIN_BUFFER_SIZE = 1024; // 1K
 
     /**
      * @var int Max buffer size
      */
-    const MAX_BUFFER_SIZE = 5120000; // 5M
+    public const MAX_BUFFER_SIZE = 5120000; // 5M
 
     /**
      *  @var int Default buffer size
      */
-    const DEFAULT_BUFFER_SIZE = 5120; // 5K
+    public const DEFAULT_BUFFER_SIZE = 5120; // 5K
 
     /**
      * @var string Default file path to which logger write
      */
-    const DEFAULT_FILE = 'php://stderr';
+    public const DEFAULT_FILE = 'php://stderr';
 
     /**
      * @var string Buffer in which logger store messages
@@ -50,41 +52,52 @@ class Buffered extends Minimal
     protected $fd;
 
     /**
-     * Create a new instance of \Log\Bufferd
+     * Create a new instance of Log\Buffered
      *
      * @param array $attrs
-     * @throws \Exception
+     * @throws InvalidArgumentException
      */
-    function __construct($attrs = array())
+    public function __construct($attrs = [])
     {
-        if (isset($attrs['color']))
+        if (isset($attrs['color'])) {
             parent::$color = $attrs['color'];
-        if (isset($attrs['debug']))
+        }
+        if (isset($attrs['debug'])) {
             parent::$debug = $attrs['debug'];
-        if (isset($attrs['log_level']))
+        }
+        if (isset($attrs['log_level'])) {
             parent::$log_level = $attrs['log_level'];
-        if (isset($attrs['trace_level']))
+        }
+        if (isset($attrs['trace_level'])) {
             parent::$trace_level = $attrs['trace_level'];
-
-        if (isset($attrs['file']) && file_exists($attrs['file'])) {
-            if (!is_readable($attrs['file']))
-                throw new \Exception(sprintf('%s is not readable', $attrs['file']));
-            if (!is_writable($attrs['file']))
-                throw new \Exception(sprintf('%s is not writable', $attrs['file']));
         }
 
-        if (isset($attrs['file'])) $this->file = $attrs['file'];
+        if (isset($attrs['file']) && file_exists($attrs['file'])) {
+            if (!is_readable($attrs['file'])) {
+                throw new InvalidArgumentException(sprintf('%s is not readable', $attrs['file']));
+            }
+            if (!is_writable($attrs['file'])) {
+                throw new InvalidArgumentException(sprintf('%s is not writable', $attrs['file']));
+            }
+        }
+
+        if (isset($attrs['file'])) {
+            $this->file = $attrs['file'];
+        }
         $this->fd = fopen($this->file, 'a+');
 
-        if (isset($attrs['buffer_size']) && $attrs['buffer_size'] < self::MIN_BUFFER_SIZE)
-            throw new \Exception('buffer_size must be more than ' . self::MIN_BUFFER_SIZE);
-        if (isset($attrs['buffer_size']) && $attrs['buffer_size'] > self::MAX_BUFFER_SIZE)
-            throw new \Exception('buffer_size must be less than ' . self::MAX_BUFFER_SIZE);
-        if (isset($attrs['buffer_size']))
+        if (isset($attrs['buffer_size']) && $attrs['buffer_size'] < self::MIN_BUFFER_SIZE) {
+            throw new InvalidArgumentException('buffer_size must be more than ' . self::MIN_BUFFER_SIZE);
+        }
+        if (isset($attrs['buffer_size']) && $attrs['buffer_size'] > self::MAX_BUFFER_SIZE) {
+            throw new InvalidArgumentException('buffer_size must be less than ' . self::MAX_BUFFER_SIZE);
+        }
+        if (isset($attrs['buffer_size'])) {
             $this->buffer_size = $attrs['buffer_size'];
-        
+        }
+
         $obj = $this;
-        parent::$print = function($time, $type, $msg, $trace, $raw_msg) use ($obj)
+        parent::$print = static function($time, $type, $msg, $trace, $raw_msg) use ($obj)
         {
             $obj->append(sprintf("%s [%s] %s at %s line %s\n",
                     $time, $type, $msg, $trace['file'], $trace['line']));
@@ -97,7 +110,7 @@ class Buffered extends Minimal
      * @param string $message A message
      * @return void
      */
-    public function append($message)
+    public function append($message): void
     {
         $this->buffer .= $message;
         if ($this->getBufferedSize() > $this->buffer_size) {
@@ -110,7 +123,7 @@ class Buffered extends Minimal
      *
      * @return void
      */
-    public function flush()
+    public function flush(): void
     {
         fwrite($this->fd, $this->buffer);
         $this->clear();
@@ -121,45 +134,45 @@ class Buffered extends Minimal
      *
      * @return void
      */
-    public function clear()
+    public function clear(): void
     { 
         $this->buffer = '';
     }
 
     /**
      * Print the caution and flush the buffer
-     * when buffered messages reach \Log\Buffered::MAX_BUFFER_SIZE.
+     * when buffered messages reach Log\Buffered::MAX_BUFFER_SIZE.
      *
      * @return void
      */
-    protected function overflow()
+    protected function overflow(): void
     {
         $this->buffer .= sprintf(
             "***************************** CAUTION!!! **********************************\n" .
             "Flush the buffer, because the size of buffered messages reached %d.\n" .
             "If you want to buffer more messages, set the more large size to buffer_size\n" .
-            "in the constructor or by calling \Log\Buffered::setBufferSize().\n" .
+            "in the constructor or by calling Log\Buffered::setBufferSize().\n" .
             "***************************************************************************\n",
             $this->buffer_size);
         $this->flush();
     }
 
     /**
-     * Return \Log\Buffered::MIN_BUFFER_SIZE.
+     * Return Log\Buffered::MIN_BUFFER_SIZE.
      *
      * @return int
      */
-    public function getMinBufferSize()
+    public function getMinBufferSize(): int
     {
         return self::MIN_BUFFER_SIZE;
     }
 
     /**
-     * Return \Log\Buffered::MAX_BUFFER_SIZE.
+     * Return Log\Buffered::MAX_BUFFER_SIZE.
      *
      * @return int
      */
-    public function getMaxBufferSize()
+    public function getMaxBufferSize(): int
     {
         return self::MAX_BUFFER_SIZE;
     }
@@ -169,7 +182,7 @@ class Buffered extends Minimal
      *
      * @return int Size of buffer
      */
-    public function getBufferSize()
+    public function getBufferSize(): int
     {
         return $this->buffer_size;
     }
@@ -180,7 +193,7 @@ class Buffered extends Minimal
      * @param int $size Size of the buffer
      * @return void
      */
-    public function setBufferSize($size)
+    public function setBufferSize($size): void
     {
         $this->buffer_size = $size;
     }
@@ -188,10 +201,13 @@ class Buffered extends Minimal
     /**
      * Return current size of buffered messages.
      *
-     * @return int Current size of bffered messages
+     * @return int Current size of buffered messages
      */
-    public function getBufferedSize()
+    public function getBufferedSize(): int
     {
+        if ($this->buffer === null) {
+            return 0;
+        }
         return strlen($this->buffer);
     }
 
@@ -200,7 +216,7 @@ class Buffered extends Minimal
      *
      * @return string The path to file to which logger print messages
      */
-    public function getFile()
+    public function getFile(): string
     {
         return $this->file;
     }
@@ -210,8 +226,10 @@ class Buffered extends Minimal
      *
      * @return void
      */
-    function __destruct()
+    public function __destruct()
     {
-        if ($this->getBufferedSize() > 0) $this->flush();
+        if ($this->getBufferedSize() > 0) {
+            $this->flush();
+        }
     }
 }
